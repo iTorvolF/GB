d45ab7a7053eb33caa40f14312c1b7804e8306c2
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-09-19 10:21:53 +0300

    Add files via upload

---
+++ Part2/Lesson2/colsort.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+arr = []
+
+loop do
+  print 'Введите цвет или stop: '
+  color = gets.strip
+  break if color == 'stop'
+
+  arr << color
+end
+
+puts arr.sort.uniq
