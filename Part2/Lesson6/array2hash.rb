1f510837cbfd8864fc66910f8097ddc87457db55
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-10-06 20:01:01 +0300

    Update array2hash.rb

--- Part2/Lesson6/array2hash.rb
+++ Part2/Lesson6/array2hash.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
-arr = [[:first, 1.to_s.to_sym], [:second, 2.to_s.to_sym], [:third, 3.to_s.to_sym]].to_h
-puts arr
+a1 = %i[first second third]
+a2 = [1, 2, 3]
+p arr = a1.zip(a2).to_h
