eaa6ebb3d46c07553030588e18a0f41938331239
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-10-07 18:34:22 +0300

    Delete arrhash.rb

--- arrhash.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-a = %w[red orange yellow green gray indigo violet]
-b = %w[красный оранжевый желтый зеленый голубой синий фиолетовый]
-
-p a.map(&:to_sym).zip(b).to_h
