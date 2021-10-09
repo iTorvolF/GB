86ed7469e1a761a73b5c7119e0c08c2a1d6ab162
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-10-09 13:59:29 +0300

    Add files via upload

---
+++ Part2/Course2/calendar.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'date'
+
+head = Date.today.strftime('%B %Y')
+year = Date.today.year
+month = Date.today.mon
+firstday = Date.new(year, month, 4).day
+lastday = Date.new(year, month, -1).day
+week = %w[пн вт ср чт пт сб вс]
+
+puts head.center(20)
+puts week.join(' ')
+print '   ' * firstday
+
+days = firstday
+(1..lastday).select do |date|
+  print "#{date.to_s.rjust(2)} "
+  days += 1
+  print "\n" if (days % 7).zero?
+end
