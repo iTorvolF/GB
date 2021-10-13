a07bc1fe2b462a2b55bbf84ee8c7797b1a93f903
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-10-13 18:34:45 +0300

    Update user.rb

--- Part2/Lesson7/user.rb
+++ Part2/Lesson7/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User
-  attr_accessor :last_name, :firts_name, :mid_name, :email
+  attr_accessor :last_name, :first_name, :mid_name, :email
 
   def initialize
     yield self
@@ -10,12 +10,12 @@ end
 
 user = User.new do |t|
   t.last_name = 'Ivanov'
-  t.firts_name = 'Ivan'
+  t.first_name = 'Ivan'
   t.mid_name = 'Ivanovich'
   t.email = 'ivanov@mail.ru'
 end
 
 p user.last_name
-p user.firts_name
+p user.first_name
 p user.mid_name
 p user.email
