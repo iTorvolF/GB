0877ed2abd63135edec526a1917521c6c6d4b054
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-10-29 20:32:56 +0300

    Add files via upload

---
+++ Part3/Lesson3/colors.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+COLORS = {
+  red: 'красный',
+  orange: 'оранжевый',
+  yellow: 'желтый',
+  green: 'зеленый',
+  blue: 'голубой',
+  indigo: 'синий',
+  violet: 'фиолетовый'
+}.freeze
+
+COLORS.each do |eng, rus|
+  define_method eng do
+    rus
+  end
+end
+
+puts green
+puts yellow
