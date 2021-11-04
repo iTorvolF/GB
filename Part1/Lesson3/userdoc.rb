5a3d193e63fe8b323ca73ef496c1711caecf7f22
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-11-04 22:51:50 +0300

    Add files via upload

---
+++ Part1/Lesson3/userdoc.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+##
+# Класс пользователь
+class User
+  ##
+  # Выводит в стандартный поток имя и профессию пользователя
+  #
+  # = Пример использования
+  #
+  # user = User.new
+  #
+  # user.fio
+  #
+  # user.profession
+  def fio
+    'Иванов Иван Иванович'
+  end
+
+  def profession
+    'Инженер'
+  end
+end
+
+user = User.new
+
+puts "Фамилия Имя Отчество: #{user.fio}"
+puts "Профессия: #{user.profession}"
