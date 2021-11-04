268accf81ede743df0ac9187cb276ac0f74c6514
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-11-04 22:52:25 +0300

    Add files via upload

---
+++ Part1/Lesson4/Pi.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+str = '3.1415926535'
+puts str[0, 4]
+puts "#{str[0]}#{str[1]}#{str[2]}#{str[3]}"
---
+++ Part1/Lesson4/acol.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+colors = %w[Красный Оранжевый Жёлтый Зелёный Голубой Синий Фиолетовый]
+puts colors.sample
