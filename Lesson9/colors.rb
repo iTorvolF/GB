704aba972acd3d13a45fa5247fb7037736017ccb
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-11-04 22:58:23 +0300

    Delete Lesson9 directory

--- Lesson9/colors.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-C = Hash[
-  'red' => 1, 'orange' => 2, 'yellow' => 3,
-  'green' => 4, 'indigo' => 5, 'blue' => 6, 'violet' => 7
-]
-print 'Введите цвет: '
-puts (C[gets.chomp]).to_i
--- Lesson9/far2cel.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-def convert(f)
-  (f - 32) * 5 / 9
-end
-
-puts 'Введите температуру по фаренгейту: '
-puts "Температура по цельсию: #{convert(gets.chomp.to_i)}"
