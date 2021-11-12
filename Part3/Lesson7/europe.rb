d9d66c62e98f4b091901c112d5dacdc77fa515db
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-11-12 19:51:21 +0300

    Add files via upload

---
+++ Part3/Lesson7/europe.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+coutries = {
+  Russia: 146_171_015,
+  Germany: 83_155_031,
+  England: 67_081_000,
+  France: 64_844_037,
+  Italy: 59_257_566,
+  Spain: 47_363_419,
+  Ukraine: 41_418_717,
+  Poland: 37_899_070,
+  Romania: 19_257_520,
+  Netherlands: 17_441_500
+}
+
+p coutries
