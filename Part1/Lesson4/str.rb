7fb420ce4e31f8a43067dbf33e43164f5a6a32d5
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-13 20:25:15 +0300

    Update str.rb

--- Part1/Lesson4/str.rb
+++ Part1/Lesson4/str.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# По документации Ruby изучите методы класса String. 
+# Подберите метод, который переворачивает строку. 
+# Например, превращает 'Hello' в 'olleH'.
+
 str = 'Hello!'
 puts str.reverse
 puts "#{str[5]}#{str[4]}#{str[3]}#{str[2]}#{str[1]}#{str[0]}"
