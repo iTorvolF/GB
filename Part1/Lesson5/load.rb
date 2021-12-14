e9326f8e5f3ee28409312668d58a2d2ba0e3be1e
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-14 19:57:59 +0300

    Update load.rb

--- Part1/Lesson5/load.rb
+++ Part1/Lesson5/load.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Напишите программу, которая подсчитывает количество элементов в массиве $LOAD_PATH.
+
 puts $LOAD_PATH
 puts $LOAD_PATH.count
 puts $LOAD_PATH.size
