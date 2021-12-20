94708da4c54fc88a522fc854879ac99b8ead1772
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-20 21:04:21 +0300

    Update aselect.rb

--- Part2/Lesson2/aselect.rb
+++ Part2/Lesson2/aselect.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Есть массив %w[cat dog tiger]. 
+# Верните массив, состоящий только из элементов, где встречается символ t.
+
 arr = %w[cat dog tiger]
 result = arr.select { |a| a.include? 't' }
 
