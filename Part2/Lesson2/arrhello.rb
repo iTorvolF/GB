d0cc768f763d38314de07fff69f39ed7f35252b3
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-20 21:05:57 +0300

    Update arrhello.rb

--- Part2/Lesson2/arrhello.rb
+++ Part2/Lesson2/arrhello.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Строку Hello world! преобразуйте в массив ["H", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d", "!"]. 
+# Выражение преобразования должно быть однострочным.
+
 str = 'Hello World!'
 
 puts str.split(',')
