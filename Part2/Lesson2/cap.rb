ea605dd07e8bd0ab587f75d6b01d5ffb70367b2e
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-20 21:04:41 +0300

    Update cap.rb

--- Part2/Lesson2/cap.rb
+++ Part2/Lesson2/cap.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Есть массив %w[cat dog tiger]. 
+# Измените его так, чтобы все элементы были написаны с заглавной буквы.
+
 arr = %w[cat dog tiger]
 result = arr.map(&:capitalize)
 
