8700f5f10f90e52b149033cb665995d66b96045f
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-21 19:06:03 +0300

    Update times.rb

--- Part2/Lesson4/times.rb
+++ Part2/Lesson4/times.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# Добавьте в стандартный класс Integer методы minutes, hours, days, 
+# которые возвращают количество секунд, соответствующих заданным значениям. 
+# Например, вызов 5.minutes должен вернуть 300, 2.hours — 7200, а 1.days — 86400.
+
 class Integer
   MINUTES = 60
   HOURS = 3600
