73be120e509634f3c8bb4b93f25560b5e96462db
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-22 19:07:22 +0300

    Update array2hash.rb

--- Part2/Lesson6/array2hash.rb
+++ Part2/Lesson6/array2hash.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# Дан массив:
+# %i(first second third)
+# Превратите его в хэш:
+# {first: 1, second: 2, third: 3}
+
 a1 = %i[first second third]
 a2 = [1, 2, 3]
 p a1.zip(a2).to_h
