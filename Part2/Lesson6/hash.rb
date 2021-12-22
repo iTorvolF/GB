721d516ecdc45fa078e7730dff7289f362b1b8f7
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-22 19:08:08 +0300

    Update hash.rb

--- Part2/Lesson6/hash.rb
+++ Part2/Lesson6/hash.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
+# Преобразуйте массив вида:
+# %w[first second third]
+# в хэш вида:
+# {first: 'Fst (1)', second: 'Snd (2)', third: 'Trd (3)'}
+# Выражение преобразования должно быть однострочным.
+
 p %w[first second third].map(&:to_sym).zip(['Fst (1)', 'Snd (2)', 'Trd (3)']).to_h
