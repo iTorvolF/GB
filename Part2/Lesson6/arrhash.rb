1f074364720519c591e28b29f2786a2d747d7874
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-22 19:09:39 +0300

    Update arrhash.rb

--- Part2/Lesson6/arrhash.rb
+++ Part2/Lesson6/arrhash.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+# Имеются два массива:
+# %w[red orange yellow green gray indigo violet]
+# и
+# %w[красный оранжевый желтый зеленый голубой синий фиолетовый]
+# Создайте из них хэш:
+# {red: 'красный', orange: 'оранжевый', yellow: 'желтый', green: 'зеленый', blue: 'голубой', indigo: 'синий', violet: 'фиолетовый'}
+
 a = %w[red orange yellow green gray indigo violet]
 b = %w[красный оранжевый желтый зеленый голубой синий фиолетовый]
 
