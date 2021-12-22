2b28e05ced3ea13fefdb1b2b78909ac17dda621b
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-22 19:04:25 +0300

    Update hello.rb

--- Part2/Lesson5/hello.rb
+++ Part2/Lesson5/hello.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Создайте класс Hello, при создании объекта которого конструктору можно было бы передавать параметр Hello.new('world'). 
+# При вызове метода say этого объекта hello.say или при выводе объекта методом puts выводилась бы фраза 'Hello, world!'.
+
 class Hello
   def initialize(name)
     @name = name.capitalize
