0314f9248be27dbe13be2ac6551a80981534a0f2
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-23 22:36:03 +0300

    Update bytes.rb

--- Part2/Lesson7/bytes.rb
+++ Part2/Lesson7/bytes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# Откройте класс Integer и добавьте в него методы kilobytes, megabytes, gigabytes и terabytes. 
+# Вызов 5.kilobytes должен вернуть количество байтов в 5-ти килобайтах — 5120. 
+# Аналогично, для остальных методов необходимо вычислить байты в соответствующем количестве мегабайт, гигабайт и терабайт.
+
 class Integer
   KILOBYTES = 1024
   MEGABYTES = 1_048_576
