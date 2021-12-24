75a9704041716a23b7728d91c487a50bbf3f90ed
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-24 23:18:16 +0300

    Update random.rb

--- Part3/Lesson1/random.rb
+++ Part3/Lesson1/random.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Создайте программу, которая принимает в качестве аргумента имя файла
+# и выводит из файла случайную строку. 
+
 def random
   File.readlines('words.txt').sample
 end
