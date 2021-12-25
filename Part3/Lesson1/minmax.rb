43b5bd7cd8e17c72e87da717c84539ffad28471f
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-25 16:20:24 +0300

    Update minmax.rb

--- Part3/Lesson1/minmax.rb
+++ Part3/Lesson1/minmax.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Создайте программу, которая выводит самую длинную и самую короткую строки файлов. 
+# Кроме того, необходимо вывести количество символов в этих строках.
+
 max_line = IO.foreach('words.txt').max_by(&:size)
 min_line = IO.foreach('words.txt').min_by(&:size)
 
