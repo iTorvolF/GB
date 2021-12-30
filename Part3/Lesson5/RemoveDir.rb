7f0caf363dd956e75b01dd94a9ebbe46666bc140
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-30 18:59:13 +0300

    Update RemoveDir.rb

--- Part3/Lesson5/RemoveDir.rb
+++ Part3/Lesson5/RemoveDir.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Создайте метод рекурсивного удаления содержимого каталога со всеми вложенными подкаталогами.
+
 require 'fileutils'
 
 FileUtils.rm_r Dir.glob('./tmp/*')
