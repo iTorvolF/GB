64c8dbc90150070b44bc54ca41c2dd1cb5925df9
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2022-01-03 22:08:03 +0300

    Update cycle.rb

--- Part3/Lesson6/cycle.rb
+++ Part3/Lesson6/cycle.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# Создайте собственный вариант итератора cycle, назвав его for. 
+# Откройте класс Array и добейтесь, 
+# чтобы новый итератор был доступен для всех массивов:
+# [1, 2, 3].cycle { |x| puts x }
+
 class Array
   def for(num = nil)
     if num.nil?
