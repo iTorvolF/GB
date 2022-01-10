1cbe24ded3015018453d84db1230c5638fbd6219
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2022-01-10 22:59:14 +0300

    Update books.rb

--- Part3/Lesson7/books.rb
+++ Part3/Lesson7/books.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
+# Пусть имеется хэш, в котором ключом выступает название книги, а значением —массив авторов:
+# authors = {
+#   'Design Patterns in Ruby' => ['Russ Olsen'],
+#   'Eloquent Ruby' => ['Russ Olsen'],
+#   'The Well-Grounded Rubyist' => ['David A. Black'],
+#   'The Ruby Programming Language' => ['David Flanagan', 'Yukihiro Matsumoto'],
+#   'Metaprogramming Ruby 2' => ['Paolo Perrotta'],
+#   'Ruby Cookbook' => ['Lucas Carlson', 'Leonard Richardson'],
+#   'Ruby Under a Microscope' => ['Pat Shaughnessy'],
+#   'Ruby Performance Optimization' => ['Alexander Dymo'],
+#   'The Ruby Way' => ['Hal Fulton', 'Andre Arko']
+# }
+# Создайте из него новый хэш, в котором ключом будет выступать автор, 
+# а значением — количество книг, которое он написал. Отсортируйте авторов по количеству книг. 
+# В группе авторов, которые написали одинаковое количество книг, отсортируйте авторов по алфавиту.
+
 BOOKS = { 'Design Patterns in Ruby' => ['Russ Olsen'],
           'Eloquent Ruby' => ['Russ Olsen'],
           'The Well-Grounded Rubyist' => ['David A. Black'],
