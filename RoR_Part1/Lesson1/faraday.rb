aab1d3d7e5addb4783105634c149817781a2ac30
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2022-04-07 23:34:39 +0300

    Update faraday.rb

--- RoR_Part1/Lesson1/faraday.rb
+++ RoR_Part1/Lesson1/faraday.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Познакомьтесь с гемом Faraday. С использованием этого гема создайте программу для обращения к странице с курсами Geekbrains https://gb.ru/courses. 
+# Подсчитайте количество строк в HTML-файле, который возвращается в ответ на запрос.
+
 require 'faraday'
 
 puts Faraday.get('https://gb.ru/courses').body.lines.count
